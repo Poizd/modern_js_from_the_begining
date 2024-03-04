cfc8512a5ff4b86e467324c952cb99065f9700ac
Poizd <muhasj@gmail.com>
2024-03-04 16:27:15 +0100

    corrected second solutin in array challenge

--- javascript-sandbox-start/02-arrays-and-objects/04-array-challenge/script.js
+++ javascript-sandbox-start/02-arrays-and-objects/04-array-challenge/script.js
@@ -17,8 +17,12 @@ const arr1 = [1, 2, 3, 4, 5]
 const arr2 = [5, 6, 7, 8, 9, 10]
 
 //solution 1
-arr3 = arr1.slice(0, 4).concat(arr2)
+//arr3 = arr1.slice(0, 4).concat(arr2)
 //solution 2
-arr3 = arr1.splice(0, 4).concat(arr2)
+//arr3 = arr1.splice(0, 4).concat(arr2)
+
+//solution 2 rewritten
+arr3 = [...arr1, ...arr2]
+arr3.splice(4, 1)
 
 console.log(arr3);
